65f5229f6c71b80af3986d96381a11ec9013d1a0
VladStarzhitskiy <vladstarzhitskiy@gmail.com>
2019-08-01 18:04:16 +0300

    l22

--- HW_22.js
+++ HW_22.js
@@ -14,7 +14,20 @@ console.log( roots );
 //#2
 
 let arr2 = [4, 3, 5, 0, 4, 8, 6];
+arr2.splice(4, 3);
 let result = arr2.reduce(function(sum, current){
-    
-});
+    return sum + current;
+}, 0);
+console.log(result);
+
+// #3
+function summArguments(){
+    let numb = 0;
+    for(let i = 0; i < arguments.length; i++){
+        numb += arguments[i];
+    }
+    return numb;
+}
+console.log(summArguments(10, 4 , 100, -80));
+
 
